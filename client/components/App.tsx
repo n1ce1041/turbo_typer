43c9ff8460f0424765246f1380137452f7d86062
Vili <viliamiktutone@gmail.com>
2024-05-07 22:37:58 +1200

    Added JS and also added Fuse to handleChange

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react'
-import '../main.css'
+import React, { useState } from 'react';
+import '../main.css';
+import Fuse from 'fuse.js'
 
 function App() {
-  const [inputValue, setInputValue] = useState('')
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  
+  const words = ["apple", "banana", "orange", "grape", "kiwi", "pineapple", "strawberry"];
+  const options = {
+    includeScore: true
   }
+  const fuse = new Fuse(words, options)
+
+  const handleChange = (event: { target: { value: any; }; }) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    // Split the input string by dots
+    const segments = value.split('.');
+  
+    // Update the search term to be the last segment after splitting by dots
+    const searchTerm = segments.pop().trim();
+    const result = fuse.search(searchTerm)
+  
+    console.log(`The current search term is ${searchTerm} with result:`);
+    console.log(result)
+
+  };
 
   return (
     <div>
@@ -19,9 +40,16 @@ function App() {
           className="input-box"
         />
       </div>
-      <div></div>
+      <div>
+        {/* Display suggestions here */}
+        <ul>
+          {suggestions.map((suggestion, index) => (
+            <li key={index}>{suggestion}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
