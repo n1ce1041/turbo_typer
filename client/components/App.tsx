bef18afcf1daec7820b5f380cf31e07418dd55da
Robert Tutone <131563383+brandhayze@users.noreply.github.com>
2024-05-07 21:23:39 +1200

    simple layout added

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,25 @@
+import React, { useState } from 'react'
+import '../main.css'
+
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <h1>Turbo Typer</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Type something here..."
+          className="input-box"
+        />
+      </div>
+      <div></div>
     </div>
   )
 }
